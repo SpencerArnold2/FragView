d99fe4771ad274e01df7d79755060e38cd6ed46a
SpencerArnold2 <spencerlarnold2@gmail.com>
2022-07-01 08:46:50 -0400

    Some testing with bond selection

---
+++ app/static/js/molpad/MPInternal.js
@@ -0,0 +1,8 @@
+/**
+ * Prints a message to the console containing bond information.
+ * @param {MPBond} aBond 
+ */
+MolPad.outputBondInfo = function (aBond) {
+    var mol = aBond.mp.mol;
+    
+}
